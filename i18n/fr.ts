8457c1c57fb6f7f0ee0bba0a851c71e9b2b6079e
Yann Voté <ygversil@lilo.org>
2020-10-09 15:39:14 +0200

    Use French as alternative locale

---
+++ i18n/fr.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../test/test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>Bonjour</translation>
+    </message>
+</context>
+<context>
+    <name>TaxrefApiAlgorithm</name>
+    <message>
+        <location filename="../taxref_api_algorithm.py" line="70"/>
+        <source>Input layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../taxref_api_algorithm.py" line="81"/>
+        <source>Output layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TaxrefApiProvider</name>
+    <message>
+        <location filename="../taxref_api_provider.py" line="75"/>
+        <source>TAXREF</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
